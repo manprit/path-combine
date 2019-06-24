955e634e81c270509ca509f1059535dc9e3a3cb8
Manprit Mann <manprit@gmail.com>
2019-06-23 21:01:16 -0700

    add babel

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ function combine(path1, path2) {
     }
     if (path1.endsWith("/")) {
         if (path2.startsWith("/")) {
-            return path1 + path2.substring(1, path2.length);
+            return `${path1}${path2.substring(1, path2.length)}`;
         }
-        return path1 + path2;
+        return `${path1}${path2}`;
     } else {
         if (path2.startsWith("/")) {
-            return path1 + path2;
+            return `${path1}${path2}`;
         }
-        return path1 + "/" + path2;
+        return `${path1}/${path2}`;
     }
 }
 
