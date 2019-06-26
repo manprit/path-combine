91118f342e16418d1d0b82bc2f65f80e3837dea2
Manprit Mann <manprit@gmail.com>
2019-06-26 00:45:28 -0700

    add tests, tweak build + test script

--- src/index.test.js
+++ src/index.test.js
@@ -2,9 +2,23 @@ var combine = require('./index');
 
 describe("combine tests", () => {
 
-    it('combines no trailing slash with no leading slash', () => {
+    it('combines paths with no trailing slash and no leading slash', () => {
         var actual = combine('http://example.com', 'test');
         expect(actual).toBe('http://example.com/test');
     })
 
+    it('combines paths with trailing slash with no leading slash', () => {
+        var actual = combine('http://example.com/', 'test');
+        expect(actual).toBe('http://example.com/test');
+    })
+
+    it('combines paths with trailing slash and leading slash', () => {
+        var actual = combine('http://example.com/', '/test');
+        expect(actual).toBe('http://example.com/test');
+    })
+
+    it('combines paths with no trailing slash and leading slash', () => {
+        var actual = combine('http://example.com', '/test');
+        expect(actual).toBe('http://example.com/test');
+    })
 })
