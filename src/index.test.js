9d56cdfe73c5c19e1b95d3a4622e115b5befcae8
Manprit Mann <manprit@gmail.com>
2020-04-10 14:38:52 -0700

    add named export

--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,29 @@
-import combine from './index';
+import { combine } from './index';
 
 describe("combine tests", () => {
 
-    it('combines paths with no trailing slash and no leading slash', () => {
-        const actual = combine('http://example.com', 'test');
-        expect(actual).toBe('http://example.com/test');
-    })
+   it('combines paths with no trailing slash and no leading slash', () => {
+      const actual = combine('http://example.com', 'test');
+      expect(actual).toBe('http://example.com/test');
+   })
 
-    it('combines paths with trailing slash with no leading slash', () => {
-        const actual = combine('http://example.com/', 'test');
-        expect(actual).toBe('http://example.com/test');
-    })
+   it('combines paths with trailing slash with no leading slash', () => {
+      const actual = combine('http://example.com/', 'test');
+      expect(actual).toBe('http://example.com/test');
+   })
 
-    it('combines paths with trailing slash and leading slash', () => {
-        const actual = combine('http://example.com/', '/test');
-        expect(actual).toBe('http://example.com/test');
-    })
+   it('combines paths with trailing slash and leading slash', () => {
+      const actual = combine('http://example.com/', '/test');
+      expect(actual).toBe('http://example.com/test');
+   })
 
-    it('combines paths with no trailing slash and leading slash', () => {
-        const actual = combine('http://example.com', '/test');
-        expect(actual).toBe('http://example.com/test');
-    })
+   it('combines paths with no trailing slash and leading slash', () => {
+      const actual = combine('http://example.com', '/test');
+      expect(actual).toBe('http://example.com/test');
+   })
 
-    it('returns path1 when path2 is not defined', () => {
-        const actual = combine('http://example.com');
-        expect(actual).toBe('http://example.com');
-    })
+   it('returns path1 when path2 is not defined', () => {
+      const actual = combine('http://example.com');
+      expect(actual).toBe('http://example.com');
+   })
 })
